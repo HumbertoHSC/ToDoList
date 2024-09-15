b9a6e007575ff10f8c741270ead4bd2d05328488
HumbertoHSC <hhsc.academico@gmail.com>
2024-09-15 11:51:44 -0300

    implementar listar tarefas

--- src/rotas.js
+++ src/rotas.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const rotas = express()
 
+const listarTarefas = require('./controladores/listarTarefas')
+
+rotas.get('/tarefa', listarTarefas)
+
 module.exports = rotas
