a16284e42113225f5ac862a8869929b6bf3c8b37
HumbertoHSC <hhsc.academico@gmail.com>
2024-09-14 23:46:38 -0300

    Organizar rotas

--- src/rotas.js
+++ src/rotas.js
@@ -2,14 +2,12 @@ const express = require('express')
 const rotas = express()
 
 const login = require('./controladores/login')
-
-rotas.post('/login', login)
-
 const cadastroUsuario = require('./controladores/cadastroUsuario')
 
-rotas.post('/cadastro', cadastroUsuario)
-
 const autenticacao = require('./intermediarios/autenticacao')
+
+rotas.post('/login', login)
+rotas.post('/cadastro', cadastroUsuario)
 
 rotas.use(autenticacao)
 
