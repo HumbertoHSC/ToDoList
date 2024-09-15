eaeed510eb4a4e1d186a891bc3f7d1acd29564f7
HumbertoHSC <hhsc.academico@gmail.com>
2024-09-14 23:58:58 -0300

    Implementar validacoes, corpo da requisicao usuario e login

--- src/rotas.js
+++ src/rotas.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const rotas = express()
 
-const login = require('./controladores/login')
-const cadastroUsuario = require('./controladores/cadastroUsuario')
+const validarCorpoReq = require('./intermediarios/validarCorpoReq')
 
 const autenticacao = require('./intermediarios/autenticacao')
+
+const schemaUsuario = require('./validacoes/schemaUsuario')
+const schemaLogin = require('./validacoes/schemaLogin')
+
+const login = require('./controladores/login')
+const cadastroUsuario = require('./controladores/cadastroUsuario')
 
 rotas.post('/cadastro', validarCorpoReq(schemaUsuario), cadastroUsuario)
 rotas.post('/login', validarCorpoReq(schemaLogin), login)
