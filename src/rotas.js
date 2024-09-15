ac20fbe4f706770475fbc2c54a99fe1fe16c74bc
HumbertoHSC <hhsc.academico@gmail.com>
2024-09-15 11:55:03 -0300

    Atualizar rotas

--- src/rotas.js
+++ src/rotas.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const rotas = express()
 
-const criarTarefa = require('./controladores/criarTarefas')
-
-rotas.post('/tarefa', criarTarefa)
-
 const validarCorpoReq = require('./intermediarios/validarCorpoReq')
 
 const autenticacao = require('./intermediarios/autenticacao')
@@ -15,6 +11,7 @@ const schemaLogin = require('./validacoes/schemaLogin')
 const login = require('./controladores/login')
 const cadastroUsuario = require('./controladores/cadastroUsuario')
 
+const criarTarefa = require('./controladores/criarTarefas')
 const listarTarefas = require('./controladores/listarTarefas')
 
 rotas.post('/cadastro', validarCorpoReq(schemaUsuario), cadastroUsuario)
@@ -22,6 +19,7 @@ rotas.post('/login', validarCorpoReq(schemaLogin), login)
 
 rotas.use(autenticacao)
 
+rotas.post('/tarefa', criarTarefa)
 rotas.get('/tarefa', listarTarefas)
 
 module.exports = rotas
